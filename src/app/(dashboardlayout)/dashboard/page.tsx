283763b28eaab6d2b1dbffc36e89387a9253424b
Nagendra Manukonda <nagendra.manukonda@jsspro.com>
2025-10-06 12:40:10 +0530

    Loading Dashboard...

--- src/app/(dashboardlayout)/dashboard/page.tsx
+++ src/app/(dashboardlayout)/dashboard/page.tsx
@@ -1,33 +1,11 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
+
 import DashboardMain from "@/components/DashboardMain";
 
 export default function DashboardLayout() {
-  const router = useRouter();
-  const [isAuthChecked, setIsAuthChecked] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const token = Cookies.get("token");
-
-    if (!token) {
-      router.replace("/login");
-    } else {
-      setIsAuthenticated(true);
-    }
-
-    setIsAuthChecked(true);
-  }, [router]);
-
-  if (!isAuthChecked) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        Loading Dashboard...
-      </div>
-    );
-  }
-
-  return isAuthenticated ? <DashboardMain /> : null;
+  return (
+    <div className="">
+      <DashboardMain />
+    </div>
+  );
 }
