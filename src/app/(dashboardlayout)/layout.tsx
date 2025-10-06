c2ad1dcda2f55f5eee9a15e4f4e23ff71a82eb94
Nagendra Manukonda <nagendra.manukonda@jsspro.com>
2025-10-06 15:43:32 +0530

    layout

--- src/app/(dashboardlayout)/layout.tsx
+++ src/app/(dashboardlayout)/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 import DashboardSidebar from "@/components/DashboardSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
@@ -10,6 +12,27 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   const [collapsed, setCollapsed] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [checkingAuth, setCheckingAuth] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+
+    if (!token) {
+      router.push("/login");
+    } else {
+      setIsAuthenticated(true);
+    }
+
+    setCheckingAuth(false);
+  }, [router]);
+
+  if (checkingAuth) {
+    return <div className="h-screen w-screen bg-white" />; // Blank screen while checking
+  }
+
+  if (!isAuthenticated) return null;
 
   return (
     <SidebarProvider>
